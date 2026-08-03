85344ac856f0ffe07ca805f87423ebc0c6927897
Nicolas Geyer <nicolasgeyer@gmx.de>
2026-08-03 18:58:54 +0200

    "Standard"-Settings & Schwierigkeitsgrade erstellt

---
+++ app/src/main/java/com/n1cologne/quizgame/domain/model/Difficulty.kt
@@ -0,0 +1,8 @@
+package com.n1cologne.quizgame.domain.model
+
+enum class Difficulty {
+    EASY,
+    MEDIUM,
+    HARD,
+    //ANY (optional für Fragen mit random Schwierigkeitsgrad)
+}
--- app/src/main/java/com/n1cologne/quizgame/navigation/AppStart.kt
+++ app/src/main/java/com/n1cologne/quizgame/navigation/AppStart.kt
@@ -1,6 +1,17 @@
 package com.n1cologne.quizgame.navigation
 
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Icon
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -15,21 +26,51 @@ fun AppStart(
 ) {
     val navController = rememberNavController()
 
-    NavHost(
-        navController = navController,
-        startDestination = QuizGameRoute,
-        modifier = modifier
-    ) {
-        composable<SettingsRoute> {
-            SettingsScreen()
-        }
+    var selectedTab by rememberSaveable {
+        mutableStateOf(TabItem.QUIZ_GAME)
+    }
 
-        composable<QuizGameRoute> {
-            QuizGameScreen()
+    Scaffold(
+        modifier = modifier.fillMaxSize(),
+        bottomBar = {
+            NavigationBar {
+                TabItem.entries.forEach { tabItem ->
+                    NavigationBarItem(
+                        selected = selectedTab == tabItem,
+                        onClick = {
+                            selectedTab = tabItem
+                        },
+                        icon = {
+                            Icon(
+                                imageVector = tabItem.tabIcon,
+                                contentDescription = tabItem.tabTitle
+                            )
+                        },
+                        label = {
+                            Text(text = tabItem.tabTitle)
+                        }
+                    )
+                }
+            }
         }
+    ) { innerPadding ->
 
-        composable<ResultRoute> {
-            ResultScreen()
+        NavHost(
+            navController = navController,
+            startDestination = selectedTab.route,
+            modifier = Modifier.padding(innerPadding)
+        ) {
+            composable<SettingsRoute> {
+                SettingsScreen()
+            }
+
+            composable<QuizGameRoute> {
+                QuizGameScreen()
+            }
+
+            composable<ResultRoute> {
+                ResultScreen()
+            }
         }
     }
 }
