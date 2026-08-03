ddf92a0efb895cfc33c613b079bff4d0372c3b13
Nicolas Geyer <nicolasgeyer@gmx.de>
2026-08-03 20:10:10 +0200

    QuizUiState & QuizViewModel

---
+++ app/src/main/java/com/n1cologne/quizgame/ui/screen/QuizUiState.kt
@@ -0,0 +1,13 @@
+package com.n1cologne.quizgame.ui.screen
+
+import com.n1cologne.quizgame.domain.model.QuizQuestion
+
+data class QuizUiState(
+    val isLoading: Boolean = false,
+    val questions: List<QuizQuestion> = emptyList(),
+    val currentQuestionIndex: Int = 0,
+    val selectedAnswer: String? = null,
+    val correctAnswers: Int = 0,
+    val isQuizFinished: Boolean = false,
+    val errorMessage: String? = null
+)
---
+++ app/src/main/java/com/n1cologne/quizgame/ui/screen/QuizViewModel.kt
@@ -0,0 +1,51 @@
+package com.n1cologne.quizgame.ui.screen
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.n1cologne.quizgame.domain.repository.QuizMockRepository
+import com.n1cologne.quizgame.domain.model.QuizSettings
+import com.n1cologne.quizgame.domain.repository.QuizRepository
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+
+class QuizViewModel(
+    private val repository: QuizRepository = QuizMockRepository()
+) : ViewModel() {
+
+    private val _uiState = MutableStateFlow(
+        QuizUiState()
+    )
+
+    val uiState: StateFlow<QuizUiState> =
+        _uiState.asStateFlow()
+
+    init {
+        loadQuestions()
+    }
+
+    fun loadQuestions(
+        settings: QuizSettings = QuizSettings()
+    ) {
+        viewModelScope.launch {
+            _uiState.value = _uiState.value.copy(
+                isLoading = true,
+                errorMessage = null
+            )
+
+            repository.getQuestions(settings)
+                .onSuccess { questions ->
+                    _uiState.value = QuizUiState(
+                        questions = questions
+                    )
+                }
+                .onFailure { error ->
+                    _uiState.value = QuizUiState(
+                        errorMessage = error.message
+                            ?: "Die Fragen konnten nicht geladen werden."
+                    )
+                }
+        }
+    }
+}
