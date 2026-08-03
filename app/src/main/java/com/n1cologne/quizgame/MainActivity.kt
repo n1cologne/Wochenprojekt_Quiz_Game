433b0f2b81804c94f6fb730f4bdc120895e0cbd7
Nicolas Geyer <nicolasgeyer@gmx.de>
2026-08-03 18:21:03 +0200

    Previews zu SettingsScreen & QuizGameScreen (& ResultScreen) erstellt

--- app/src/main/java/com/n1cologne/quizgame/MainActivity.kt
+++ app/src/main/java/com/n1cologne/quizgame/MainActivity.kt
@@ -4,44 +4,34 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.n1cologne.quizgame.navigation.AppStart
 import com.n1cologne.quizgame.ui.theme.QuizGameTheme
 
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
         setContent {
             QuizGameTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                AppStart()
             }
         }
     }
 }
 
+@Preview(
+    showBackground = true,
+    showSystemUi = true
+)
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
+private fun MainActivityPreview() {
     QuizGameTheme {
-        Greeting("Android")
+        AppStart()
     }
 }
