5624a12164e4ce38ea8a49c40a12b6a8e6d002c6
Nicolas Geyer <nicolasgeyer@gmx.de>
2026-08-03 17:22:03 +0200

    Screens & Navigation-Route-Struktur erstellt

---
+++ app/src/main/java/com/n1cologne/quizgame/navigation/Routes.kt
@@ -0,0 +1,12 @@
+package com.n1cologne.quizgame.navigation
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+object SettingsRoute
+
+@Serializable
+object QuizGameRoute
+
+@Serializable
+object ResultRoute
